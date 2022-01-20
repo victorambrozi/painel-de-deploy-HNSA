ccaaf6a6216d33401a0eb2bb0ea950fe41ce9135
Victor Ambrozi <victorambrozi.dev@gmail.com>
2022-01-20 19:25:47 -0300

    delete README

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Login from "./pages/Login/Login";
 import { GlobalStyle } from "./styles/GlobalStyle.js";
+import PainelDeDeploys from "./pages/PainelDeDeploys/PainelDeDeploys"
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Login />
+      <PainelDeDeploys />
     </>
   );
 }
--- src/pages/PainelDeDeploys/PainelDeDeploys.jsx
+++ src/pages/PainelDeDeploys/PainelDeDeploys.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PainelDeDeploys = () => {
-  return <div></div>;
+  return <div>PainelDeDeploys</div>;
 };
 
 export default PainelDeDeploys;
